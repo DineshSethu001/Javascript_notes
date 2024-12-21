6e9bb3712a056d03803f92604e471a94a19f755f
Dineshsethu001 <dineshsethu15981@gmail.com>
2024-12-21 15:27:41 +0530

    Code challenge:(Display the character the in your name one by one)

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+// Code challenge:(Display the character the in your name one by one)
+
+let user_name = "Dinesh Thanigaivel"
+let randome = Math.floor(Math.random()* user_name.length)
+console.log("", user_name.charAt(randome))
